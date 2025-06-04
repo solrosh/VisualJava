d569f996f4ee2c59803788d19c71fe1d1f7fdfdf
solrosh <juanjose2020@hotmail.es>
2025-06-04 10:02:43 +0200

    Nuevo Ejercicio 04

---
+++ Class04PositivoNegativo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Class04PositivoNegativo {
+    public static void main(String[] strg){
+        Scanner read = new Scanner(System.in);
+        int numero1;
+
+        System.out.print("Introduzca un número: ");
+        numero1 = read.nextInt();
+
+        if (numero1 > 0) {
+            System.out.println("El número es POSITIVO");
+        }
+        else if (numero1 == 0){
+            System.out.println("El número es CERO");
+        }
+        else
+            System.out.println("El número es NEGATIVO");
+    }
+}
